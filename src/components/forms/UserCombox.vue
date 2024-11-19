38e89f4f0799da0f9bc672fbf8f699d62779efc0
Carlos <casalgado86@gmail.com>
2024-11-19 07:04:02 -0500

    UserCombox v2

--- src/components/forms/UserCombox.vue
+++ src/components/forms/UserCombox.vue
@@ -8,7 +8,7 @@ import {
   ComboboxOption,
   TransitionRoot,
 } from '@headlessui/vue';
-import { PhCaretDown } from '@phosphor-icons/vue';
+import { PhCaretUpDown } from '@phosphor-icons/vue';
 
 const props = defineProps({
   modelValue: {
@@ -58,30 +58,35 @@ const handleSelect = (userId) => {
       @update:modelValue="handleSelect"
     >
       <div class="relative">
-        <ComboboxInput
-          @change="query = $event.target.value"
-          :displayValue="(userId) => props.users.find(u => u.id === userId)?.name || ''"
-        />
-        <ComboboxButton>
-          <PhCaretDown />
-        </ComboboxButton>
+        <div class="relative w-full">
+          <ComboboxInput
+            @change="query = $event.target.value"
+            :displayValue="(userId) => props.users.find(u => u.id === userId)?.name || ''"
+          />
+          <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
+            <PhCaretUpDown />
+          </ComboboxButton>
+        </div>
 
         <TransitionRoot
-          leave="transition ease-in duration-100"
+          leave="transition ease-in duration-200"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <ComboboxOptions>
+          <ComboboxOptions class="absolute mt-1 w-full  rounded-md bg-white py-1  shadow-lg text-[13px] z-10">
             <ComboboxOption
               v-for="user in filteredUsers"
               :key="user.id"
               :value="user.id"
               v-slot="{ selected, active }"
+
             >
-              <div :class="{
-                'bg-blue-500 text-white': active,
-                'font-bold': selected
-              }">
+              <div
+                class="cursor-default select-none  pl-2 py-1"
+                :class="{
+                  'bg-blue-500 text-white': active,
+                  'font-bold': selected
+                }">
                 {{ user.name }}
               </div>
             </ComboboxOption>
